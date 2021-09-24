d809c1c76c3eb8aefef3293906c98f36e608b9eb
ntoliverapp <n.toliverapplication@gmail.com>
2021-09-24 11:56:24 -0700

    password updated

--- password.js
+++ password.js
@@ -1,27 +1,28 @@
 function welcome(newUser) {
   console.log(`Welcome, ${newUser}!`)
-  console.log('Please enter a password that is 10 charaters long (with uppercase and special characters,): ') 
+  console.log('Please enter a password that is 10 characters long (with uppercase and special characters,): ') 
 }
 function createPassWord(passWord) {
   let passWordSplit = passWord.split('')
   console.log(passWordSplit)
 
   let passWordSpecial = ['@', '#', '%', '$', '%', '^', '&', '&', '*', '(', ')']
-  for (let i = 0; i < passWordSpecial.length; i++){
-    if(passWordSpecial[i].includes === true) {
-      //console.log('Sorry, please include special characters in your password')
-      break
-    }
-  }
+
+  for (let i = 0; i < passWordSpecial.length; i++) {
   
-  if(passWordSplit.length >= 10 !== true) {
+  if(passWordSplit.length >= 10 === true && passWordSpecial.includes(passWordSpecial[i]) === true) {
+    console.log(`Your password is good to go!`)
+    break
+  } else if(passWordSplit.length >= 10 === true && passWordSpecial.includes(passWordSpecial[i]) !== true) {
+    console.log('Sorry, please include special characters in your password')
+    break
+   } else if(passWordSplit.length >= 10 !== true && passWordSpecial.includes(passWordSpecial[i]) === true) {
     console.log(`Sorry please type a password that is 10 characters long`)
-  //} else if(passWordSplit.includes('@') !== true) {
-    //console.log('Sorry, please include special characters in your password')
-  } else {
-      console.log(`Your password is good to go!`)
+    break
+  //console.log(`Your password is good to go!`)
   }
 }
+}
 let website = welcome('Niq')
-let passwordAuthenticator = createPassWord("Whngg() dkgj")
+let passwordAuthenticator = createPassWord("Wh$gggdkgj")
 
