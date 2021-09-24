a7b18008c03a867664fd2a928535b7ec0e6314d6
ntoliverapp <n.toliverapplication@gmail.com>
2021-09-24 11:31:23 -0700

    updated password

--- password.js
+++ password.js
@@ -1,18 +1,27 @@
 function welcome(newUser) {
   console.log(`Welcome, ${newUser}!`)
-  console.log('Please enter a password: ') 
+  console.log('Please enter a password that is 10 charaters long (with uppercase and special characters,): ') 
+}
+function createPassWord(passWord) {
+  let passWordSplit = passWord.split('')
+  console.log(passWordSplit)
 
-  function createPassWord(passWord) {
-    passWordSplit = passWord.split('')
-    console.log(passWordSplit)
-    if(passWordSplit.length > 10) {
-      console.log(`Your password is good to go ${newUser}`)
-    } else {
-      console.log(`Sorry ${newUser} please type a password that is 10 characters long`)
+  let passWordSpecial = ['@', '#', '%']
+  for (let i = 0; i < passWordSpecial.length; i++){
+    if(passWordSpecial[i].includes !== true) {
+      console.log('Sorry, please include special characters in your password')
+      break
     }
   }
+  
+  if(passWordSplit.length >= 10 !== true) {
+    console.log(`Sorry please type a password that is 10 characters long`)
+  //} else if(passWordSplit.includes('@') !== true) {
+    //console.log('Sorry, please include special characters in your password')
+  //} else {
+  //    console.log(`Your password is good to go!`)
+  }
 }
-
-welcome('Niq')
-createPassWord("Whdhforn fldkgj")
+let website = welcome('Niq')
+let passwordAuthenticator = createPassWord("Whngg dkgj")
 
